6d0294f780433f468d6c25e7e11e929e367c4308
이상현 <tkdgus3769@naver.com>
2024-11-01 16:36:16 +0900

    프로젝트 sql 추가

---
+++ Java_Practice2/src/Lsh0708_project/project.sql
@@ -0,0 +1,20 @@
+CREATE SEQUENCE id_seq
+START WITH 1
+INCREMENT BY 1;
+
+CREATE TABLE employee (
+id NUMBER PRIMARY KEY,
+name VARCHAR(100),
+department VARCHAR(100),
+birthdate VARCHAR(100),
+address VARCHAR(100),
+telNum VARCHAR(30),
+sex VARCHAR(10)
+);
+insert into employee (id,name,department,birthdate,address,telNum,sex) values(1,'lsh','Research',
+'1995-07-08','Busan','010-1234-4567','Male');
+
+
+select * from employee;
+
+commit;
